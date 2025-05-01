a382039bab6b578b5eb071378dde6b497906b881
ffilopeter <33666956+ffilopeter@users.noreply.github.com>
2025-05-01 14:34:00 +0200

    load TreeView with selected folder structure

--- main.ps1
+++ main.ps1
@@ -1,6 +1,10 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+# =======================
+# GUI
+# =======================
+
 # Create Form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Folder Tree with Permissions Viewer"
@@ -35,6 +39,74 @@ $browseButton.Location = New-Object System.Drawing.Point(10, 10)
 $browseButton.Anchor = 'Top, Left'
 
 $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+
+# =======================
+# Event handlers
+# =======================
+
+# Click to Browse button
+# load folder structure to tree view
+$browseButton.Add_Click({
+    if ($folderBrowser.ShowDialog() -eq "OK") {
+
+        # Clear treeView and listView
+        $treeView.Nodes.Clear()
+        $listView.Items.Clear()
+
+        $rootNode = New-Object System.Windows.Forms.TreeNode
+        $rootNode.Text = [System.IO.Path]::GetFileName($folderBrowser.SelectedPath)
+        $rootNode.Tag = $folderBrowser.SelectedPath
+        $rootNode.ForeColor = [System.Drawing.Color]::Blue
+
+        $treeView.Nodes.Add($rootNode)
+
+        # Load the whole tree
+        Load-TreeView -parentNode $rootNode -path $folderBrowser.SelectedPath
+        
+        $rootNode.Expand()
+    }
+})
+
+# =======================
+# Functions
+# =======================
+
+# Load TreeView recursively
+function Load-TreeView {
+    param([System.Windows.Forms.TreeNode]$parentNode, [string]$path)
+
+    try {
+        
+        # Get parentNode subfolders
+        Get-ChildItem -Path $path -Directory -ErrorAction Stop | ForEach-Object {
+
+            # Add to treeView (parentNode)
+            $dirNode = New-Object System.Windows.Forms.TreeNode
+            $dirNode.Text = $_.Name
+            $dirNode.Tag = $_.FullName
+            $dirNode.ForeColor = [System.Drawing.Color]::Blue
+            $parentNode.Nodes.Add($dirNode)
+
+            # Repeat
+            Load-TreeView -parentNode $dirNode -path $_.FullName
+        }
+
+        # Get parentNode files
+        Get-ChildItem -Path $path -File -ErrorAction Stop | ForEach-Object {
+
+            # Add to treeView (parentNode)
+            $fileNode = New-Object System.Windows.Forms.TreeNode
+            $fileNode.Text = $_.Name
+            $fileNode.Tag = $_.FullName
+            $fileNode.ForeColor = [System.Drawing.Color]::Black
+            $parentNode.Nodes.Add($fileNode)
+        }
+    } catch {}
+}
+
+# =======================
+# Add controls and render
+# =======================
 
 $form.Controls.Add($treeView)
 $form.Controls.Add($listView)
